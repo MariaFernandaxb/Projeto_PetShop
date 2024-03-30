fa9f5250a8c727696ee3b983b2f03b7a6157f107
Nandabdev <nandab.dev@gmail.com>
2024-03-29 21:59:44 -0300

    Projeto PetShop

--- rest_api/views.py
+++ rest_api/views.py
@@ -7,7 +7,8 @@ import datetime as dt
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import api_view
+
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly, AllowAny
 
 from reservadebanho.models import Reserva
 from base.models import Contato
@@ -16,15 +17,27 @@ from rest_api.serializers import *
 class AgendamentoModelViewSet(ModelViewSet):
     queryset = Reserva.objects.all()
     serializer_class = AgendamentoModelSerializer
+    filterset_fields = {
+        'nome_pet': ['icontains'],
+        'data_reserva':['gte', 'lte']
+    }
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
 class ContatoAgendamentoModelViewSet(ModelViewSet):
     queryset = Contato.objects.all()
     serializer_class = ContatoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class FuncionarioModelViewSet(ModelViewSet):
     queryset = Funcionario.objects.all()
     serializer_class = FuncionarioSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 # @api_view([ 'POST'])
 # def hello_world(request):
--- ultima/settings.py
+++ ultima/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
     'base',
     'reservadebanho',
-    'rest_framework',
     'rest_api',
 ]
 
@@ -127,7 +129,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = { 
-    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+    'DATE_INPUT_FORMATS': ["%d/%m/%Y"],
+    'DATE_FORMAT':'%Y-%m-%d',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+         'rest_framework.authentication.SessionAuthentication',
+         'rest_framework.authentication.BasicAuthentication',
+         'rest_framework.authentication.TokenAuthentication',
+    ],
 }
